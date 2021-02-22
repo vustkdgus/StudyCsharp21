ec75d867ef06da09a0fd7b9f5bd92d1e29534d32
vustkdgus <vustkdgus@naver.com>
2021-02-22 17:05:36 +0900

    3장 모두커밋

---
+++ chap03/Chap03App/ConstTestApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConstTestApp
+{
+    class Program
+    {  
+        enum Season
+        {
+            봄,
+            여름,
+            가을,
+            겨울
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("상수테스트");
+            const double PI = 3.141592;
+            Console.WriteLine($"원주율의 값은 {PI}");
+            //PI = 6.345;
+
+            Season mySeason = Season.여름; // (Season) 3 error; // Season.여름;
+            Console.WriteLine($"지금 계절은 {mySeason} 입니다.");
+
+            int a = 0;
+            Console.WriteLine($"a는 {a}");
+            int? b = null;
+            Console.WriteLine($"b는 {b}");
+        }
+    }
+}
---
+++ chap03/Chap03App/IntConversionApp/ConvTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntConversionApp
+{
+    class ConvTest
+    {
+        static void Main(string[] args)
+        {
+            short shMaxVal = short.MaxValue;
+            int inCastVal = 0;
+
+            inCastVal = shMaxVal;
+            Console.WriteLine($"short값 변환한 int값은{inCastVal}");
+
+            inCastVal += 5;
+            short shCastVal = (short) inCastVal;
+            Console.WriteLine($"int값 변환한 short 값은{shCastVal}"); //Overflow
+
+            float flVal = 3.141592f; //f or F 써야함
+            inCastVal = (int) flVal;
+            Console.WriteLine($"float값 변환한 int값은 {inCastVal}");
+            double dlVal = inCastVal; //3
+            Console.WriteLine($"int값 변환한 double값은 {dlVal}");
+
+            object obj = 20; // Boxing
+            int inUnboxingVal = (int) obj; //Unboxing
+            Console.WriteLine($"int값 변환한 object값은 {inUnboxingVal}");
+
+            string strVal = "200";
+            int result = int.Parse(strVal) * 3; //문자열을 숫자로 변환
+            Console.WriteLine($"200 * 3 = {result}");
+        }
+    }
+}
