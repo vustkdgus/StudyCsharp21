ada543ca47b4a2563a45fad2790a7515076a30c4
vustkdgus <vustkdgus@naver.com>
2021-02-25 17:28:20 +0900

    2월 25일 커밋

---
+++ chap08/Chap08App/MultiinterfaceApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MultiinterfaceApp
+{
+    interface IRunnable // 자동차 종류
+    {
+        void Run();
+    }
+
+    interface IFlyable // 비행기 종류
+    {
+        void Fly();
+    }
+
+    class DroneCar : IRunnable, IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("날아!");
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("달려!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("드론카");
+
+            DroneCar dreamcar = new DroneCar();
+            dreamcar.Run();
+            dreamcar.Fly();
+
+            Console.WriteLine("자동차로 변경(interface)");
+            IRunnable car = dreamcar;
+            car.Run();
+            //car.Fly(); 오류
+
+            IFlyable plane = dreamcar;
+            plane.Fly();
+            //plane.Run(); 오류
+        }
+    }
+}
---
+++ chap09/Chap09App/Chap09App/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap09App
+{
+    class MyClass
+    {
+        private int iValue; // 0~120 값만 입력받는 멤버변수
+   
+
+        public int IValue 
+        { 
+            get 
+            {
+                return this.iValue;
+            }
+            set
+            {
+                if (value < 0)
+                    this.iValue = 0;
+                else if (value > 120)
+                    this.iValue = 120;
+                else
+                    this.iValue = value;
+            }
+        }
+        public MyClass(int value)
+        {
+            IValue = iValue;
+        }
+
+       /* public int GetValue()
+        {
+            return this.value;
+        }
+
+        public void Setvalue(int value)
+        {
+            if (value < 0)
+            {
+                this.value = 0;
+            }
+            else if (value > 120)
+            {
+                this.value = 120;
+            }
+            else
+            {
+                this.value = value;
+            }
+        }
+        */
+
+        public void PrintValue()
+        {
+            Console.WriteLine($"값은 {this.IValue}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass(1200);
+            myClass.PrintValue();
+
+            //myClass.Setvalue(-1300);
+            myClass.IValue = 108;
+            myClass.PrintValue();
+            Console.WriteLine($"현재 온도는 {myClass.IValue}℃ 입니다.");
+        }
+    }
+}
