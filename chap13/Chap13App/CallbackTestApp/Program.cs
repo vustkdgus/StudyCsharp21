330f9fc2c24b9a7f5dc983aa0d79bc45fae58139
vustkdgus <vustkdgus@naver.com>
2021-03-02 17:25:34 +0900

    13장 대리자 커밋

---
+++ chap13/Chap13App/CallbackTestApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallbackTestApp
+{
+    delegate int Compare(int a, int b);
+    class Program
+    {
+        // 오름차순 a > b 이면 1리턴, 같으면 0리턴, a < b 이면 -1리턴
+        static int AscendCompare(int a, int b)
+        {
+            if (a > b) return 1;  //a,b를 바꿈
+            else if (a == b) return 0;
+            else return -1;
+        }
+
+        // 내림차순 비교, a<b  1리턴, 같으면 0, a>b -1리턴
+        static int DescendCompare(int a, int b)
+        {
+            if (a < b) return 1;  //a,b를 바꿈
+            else if (a == b) return 0;
+            else return -1;
+        }
+
+        static void BubbleSort(int[] DataSet, Compare comparer)
+        {
+            int temp = 0;
+            for (int i = 0; i < DataSet.Length; i++)
+            {
+                for (int j = 0; j < DataSet.Length; j++)
+                {
+                    // 비교하여 값 위치변경
+                    if (comparer(DataSet[j], DataSet[j + 1]) > 0)
+                    {
+                        temp = DataSet[j + 1];
+                        DataSet[j + 1] = DataSet[j];
+                        DataSet[j] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 8, 4, 2, 1, 10 };
+
+            Console.WriteLine("Sorting....");
+            BubbleSort(array, new Compare(AscendCompare)); // 오름차순 정렬
+            foreach (var item in array)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
---
+++ chap13/Chap13App/Chap13App1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Chap13App1
+{
+    delegate int MyDelegate(int a, int b); // 대리자. 대신 호출한다. 호출할 메서드와 파라미터 타입/갯수가 일치해야한다.
+    class Calculator
+    {
+        public int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        public int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            MyDelegate Callback;
+
+            Callback = new MyDelegate(calc.Plus);
+            Console.WriteLine($"result = {Callback(3,4)}");
+
+            Callback = new MyDelegate(calc.Minus);
+            Console.WriteLine($"result = {Callback(5, 3)}");
+        }
+    }
+}
