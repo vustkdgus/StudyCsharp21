bcfcef1b1822e76ee7a83fb88d20e52a1489fc77
vustkdgus <vustkdgus@naver.com>
2021-03-02 15:30:42 +0900

    12장 예외처리 커밋

---
+++ chap12/Chap12App/TryCatchApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace TryCatchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+            int x = 108, y = 0;
+            int result = 0;
+
+            try
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine($"{i} 번째 값 : {arr[1]}");
+                }
+
+                result = x / y;
+                Console.WriteLine($"결과는 {result}");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+
+                Console.WriteLine($"예외발생 : {ex.Message}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"예외발생 : {ex.Message}");
+            }
+
+            Console.WriteLine($"나머지 일처리 계속...");
+
+            string strVal = "Hello World";
+            string splitVal = strVal.Substring(6, 5);
+            Console.WriteLine($"자른 문자 : {splitVal}");
+
+            try
+            {
+
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+
+                Console.WriteLine($"원본문자열이 비어있습니다. 값을 입력하세요{ex.Message}");
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine($"원본문자열을 초기화하세요{ex.Message}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"기타 예외발생{ex.Message}");
+            }
+
+            Console.WriteLine($"일처리 또 계속...");
+        }
+    }
+}
