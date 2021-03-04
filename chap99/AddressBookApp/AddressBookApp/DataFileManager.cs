864ed0481bc80c2acea0a17b8988c3149f4d946c
vustkdgus <vustkdgus@naver.com>
2021-03-04 16:13:31 +0900

    주소록 프로그램 커밋

---
+++ chap99/AddressBookApp/AddressBookApp/DataFileManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AddressBookApp
+{
+    class DataFileManager
+    {
+        const string dataFileName = "address.dat";
+
+        public List<AddressInfo> ReadData()
+        {
+            var listResult = new List<AddressInfo>();
+            var filePath = Environment.CurrentDirectory + "\\" + dataFileName; // 데이터파일
+
+            var sr = new StreamReader(new FileStream(filePath, FileMode.Open, FileAccess.Read)); //Open
+            while (sr.EndOfStream == false)
+            {
+                var temp = sr.ReadLine();
+                // temp 잘라서 manager.listAddress 할당
+                var splits = temp.Split("|");
+                listResult.Add(new AddressInfo() { Name = splits[0], Phone = splits[1], Address = splits[2] });
+            }
+            sr.Close();
+
+            return listResult;
+        }
+
+        public void WriteData(List<AddressInfo> list)
+        {
+
+            var filePath = Environment.CurrentDirectory + "\\" + dataFileName; // 데이터파일
+
+            // 주소록을 다시 파일에 씀
+            StreamWriter sw = new StreamWriter(new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write));
+            if (list.Count > 0)
+            {
+                foreach (var item in list)
+                {
+                    sw.WriteLine($"{item.Name}|{item.Phone}|{item.Address}");
+                }
+            }
+            sw.Close();
+
+        }
+    }
+}
---
+++ chap99/AddressBookApp/AddressBookApp/MainApp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AddressBookApp
+{
+
+    class MainApp
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                AddressManager manager = new AddressManager();
+                manager.listAddress = new List<AddressInfo>();
+
+                DataFileManager fileManager = new DataFileManager();
+                manager.listAddress = fileManager.ReadData();
+
+
+
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine(Environment.CurrentDirectory);
+                    manager.PrintMenu();
+                    int menuNum = manager.SelectMenu();
+
+                    switch (menuNum)
+                    {
+                        case 0: // 메뉴선택 실패, 메뉴로 돌아감
+                            break;
+
+                        case 1: // 주소입력 화면 전환
+                            manager.InputAddress();
+                            Console.Clear();
+
+                            break;
+                        case 2: // 주소검색
+                            Console.Clear();
+                            manager.SearchAddress();
+
+                            break;
+                        case 3: // 주소수정
+                            Console.Clear();
+                            manager.UpdateAddress();
+
+                            break;
+                        case 4: // 주소삭제
+                            Console.Clear();
+                            manager.DeleteAddress();
+
+                            break;
+                        case 5: // 주소 전체 출력
+                            Console.Clear();
+                            manager.PrintAllAddress();
+
+                            break;
+                        case 6: //종료
+                            fileManager.WriteData(manager.listAddress);
+                            Environment.Exit(0);
+
+                            break;
+                        default: // 0은 여기서처리
+                                 // 아무 로직 없음
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"예외발생 : {ex.Message}");
+            }
+        }
+    }
+}
